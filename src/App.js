bb4def9b6b76e7df9b54f99a62090ac224b9dbb5
Enes Eren <88148105+thenesern@users.noreply.github.com>
2022-04-24 17:24:52 +0300

    Hooks removed

--- src/App.js
+++ src/App.js
@@ -1,31 +1,50 @@
-// Dependencies
-import { useState } from "react";
 // Styles
 import styles from "./App.module.css";
 // Components
 import Hero from "./components/Hero/Hero";
-import Temperatures from "./components/Temperatures/Temperatures";
 // Data
 import data from "./api/data.json";
+import { Component } from "react";
 
-function App() {
-  // Import ettiğimiz JSON veriyi "State" olarak tanımlıyoruz.
-  const [deg, setDeg] = useState(data?.currentDeg);
+class App extends Component {
+  constructor(props) {
+    super(props);
+    // State
+    this.state = {
+      temp: data?.currentDeg,
+    };
+  }
 
-  return (
-    <div className={styles.app}>
-      <Hero data={deg} />
-      <div className={styles.buttons}>
-        <button className={styles.button} onClick={() => setDeg(deg + 1)}>
-          Sıcaklığı Artır
-        </button>
-        <button className={styles.button} onClick={() => setDeg(deg - 1)}>
-          Sıcaklığı Düşür
-        </button>
+  // Sıcaklığı artırma fonksiyonu
+  IncrementItem = () => {
+    this.setState({ temp: this.state.temp + 1 });
+    console.log(this.state.temp);
+  };
+  // Sıcaklığı düşürme fonksiyonu
+  DecreaseItem = () => {
+    this.setState({ temp: this.state.temp - 1 });
+  };
+
+  render() {
+    return (
+      <div className={styles.app}>
+        <Hero data={this.state.temp} />
+        <div className={styles.buttons}>
+          <button className={styles.button} onClick={this.IncrementItem}>
+            Sıcaklığı Artır
+          </button>
+          <button className={styles.button} onClick={this.DecreaseItem}>
+            Sıcaklığı Düşür
+          </button>
+        </div>
+        <div className={styles.cards}>
+          <p>Celcius = {this.state.temp + " °C"}</p>
+          <p>Fahrenheit = {(this.state.temp * 1.8 + 32).toFixed(1) + " °F"}</p>
+          <p>Kelvin = {this.state.temp + 273.15 + " K"}</p>
+        </div>
       </div>
-      <Temperatures data={data?.temperatures} temperature={deg} />
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
